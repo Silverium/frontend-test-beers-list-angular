c914cbc75f8a56e33b3705991ddb9e07b0cb1bc1
Soldeplata Saketos Candela <soldeplatadeveloper@gmail.com>
2019-12-16 19:13:41 +0100

    feat(BeersList):  cached saving the beers list

--- src/app/beers/store/beers.reducer.ts
+++ src/app/beers/store/beers.reducer.ts
@@ -1,5 +1,5 @@
 import { BeersState } from "./beers.state-type";
-import { FETCH_BEERS_RESPONSE, FETCH_BEER_RESPONSE, FETCH_BEER_REQUEST } from "./beers.actions";
+import { FETCH_BEERS_RESPONSE, FETCH_BEER_RESPONSE, FETCH_BEER_REQUEST, FETCH_BEERS_REQUEST } from "./beers.actions";
 import { GenericAction } from "../../models";
 
 const initialState: BeersState = {
@@ -7,13 +7,39 @@ const initialState: BeersState = {
   beer: null
 };
 const LAST_BEER = 'lastBeer';
-
+const LAST_BEERS_LIST = 'lastBeersList';
+const LAST_BEERS_QUERY = 'lastBeersQuery';
 
 export const beersReducer = (state = initialState, action: GenericAction) => {
   switch (action.type) {
     case FETCH_BEERS_RESPONSE: {
-      return <BeersState>{ ...state, beers: action.payload };
+      const beers = action.payload;
+      try {
+        localStorage.setItem(LAST_BEERS_LIST, JSON.stringify(beers));
+      } catch (error) {
+        console.info('%cvariable: error trying to save lastBeersList', 'background-color: lime;', error);
+      }
+
+      return <BeersState>{ ...state, beers };
     }
+    case FETCH_BEERS_REQUEST: {
+      try {
+        const newQuery = JSON.stringify(action.payload);
+        const lastQuery = localStorage.getItem(LAST_BEERS_QUERY);
+
+        localStorage.setItem(LAST_BEERS_QUERY, newQuery);
+        if (newQuery == lastQuery) {
+          const beers = JSON.parse(localStorage.getItem(LAST_BEERS_LIST));
+          // I assume here that localStorage is faster than network (I should make sure with timestamps)
+          return <BeersState>{ ...state, beers };
+        }
+      } catch (error) {
+        console.info(`%ccould not get last Beers list from localStorage`, 'background-color: gold;');
+
+        return state
+      }
+    }
+
     case FETCH_BEER_RESPONSE: {
       const beer = action.payload[0];
       localStorage.setItem(LAST_BEER, JSON.stringify(beer))
@@ -21,8 +47,8 @@ export const beersReducer = (state = initialState, action: GenericAction) => {
       return <BeersState>{ ...state, beer };
     }
     case FETCH_BEER_REQUEST: {
-      const id = action.payload;
       try {
+        const id = action.payload;
         const beer = JSON.parse(localStorage.getItem(LAST_BEER));
         if (beer.id == id) {
           // I assume here that localStorage is faster than network (I should make sure with timestamps)
